cfeab26d092009e23b841f2adcc2f0a27861ddaf
picronicro <eliyaz2761@gmail.com>
2023-07-14 19:53:13 +0600

    сделал секундомер

--- js/home_works.js
+++ js/home_works.js
@@ -28,3 +28,65 @@ async function moveBlock() {
 }
 
 moveBlock()
+
+// stopwatch
+// digits
+const min = document.querySelector("#minutes")
+const sec = document.querySelector("#seconds")
+const mlSeconds = document.querySelector("#ml-seconds")
+// buttons
+const btnStart = document.querySelector("#start")
+const btnStop = document.querySelector("#stop")
+const btnReset = document.querySelector("#reset")
+
+let millis = 0
+let runnableID = null;
+
+btnStart.addEventListener("click", () => {
+    if (runnableID === null) {
+        runnableID = setInterval(() => {
+            millis += 125
+            updateScreen(millis)
+        }, 125)
+    } else {
+        console.log("already running")
+    }
+})
+
+btnStop.addEventListener("click", () => {
+    clearInterval(runnableID)
+    runnableID = null
+})
+
+btnReset.addEventListener("click", () =>  {
+    if (runnableID !== null) {
+        clearInterval(runnableID)
+        runnableID = null;
+    }
+
+    clearScreen()
+    millis = 0
+})
+
+function updateScreen(millis) {
+    let ml = millis % 1000
+    let seconds = millis / 1000
+    let minutes = parseInt(seconds / 60)
+    seconds = parseInt(seconds % 60)
+
+    // console.log(`${minutes}:${seconds}:${ml}`)
+
+    min.innerHTML = ((minutes.toString().length < 2) ? "0" : "") + minutes.toString()
+    sec.innerHTML = ((seconds.toString().length < 2) ? "0" : "") + seconds.toString()
+    mlSeconds.innerHTML = ml.toString()[0] + ((ml.toString().length > 1) ? ml.toString()[1] : "0")
+}
+
+function clearScreen() {
+    min.innerHTML = "00"
+    sec.innerHTML = "00"
+    mlSeconds.innerHTML = "00"
+}
+
+
+
+
